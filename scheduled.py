a0da3c3215b77edc139b3696d8e7d4c949388e0d
Ilona <ibodnar@usc.edu>
2020-07-22 15:26:31 -0700

    test deploy

--- scheduled.py
+++ scheduled.py
@@ -2,7 +2,7 @@ import os
 import schedule
 import time
 import logging
-from slack.web.client import WebClient
+from slack import WebClient
 from slack.errors import SlackApiError
 
 logging.basicConfig(level=logging.DEBUG)
@@ -14,7 +14,7 @@ def sendMessage(slack_client, msg):
   # check if the request was a success
   try:
     slack_client.chat_postMessage(
-      channel='#test',
+      channel='#findmynerd-test',
       text=msg
     )#.get()
   except SlackApiError as e:
@@ -27,9 +27,10 @@ if __name__ == "__main__":
   logging.debug("authorized slack client")
 
   # # For testing
-  msg = "Good Morning!"
-  schedule.every(60).seconds.do(lambda: sendMessage(slack_client, msg))
-
+  msg = "Hello! I'm here to help you let your fellow nerds know where you’ll be working from next week. I’ll check in with you at 4pm every friday unless you want to change that.
+"
+  # schedule.every(60).seconds.do(lambda: sendMessage(slack_client, msg))
+  schedule.every().thursday.at("08:15").do(lambda: sendMessage(slack_client, msg))
   # schedule.every().monday.at("13:15").do(lambda: sendMessage(slack_client, msg))
   logging.info("entering loop")
 
